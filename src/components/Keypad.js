b021cee3eb029f2a1891b0cac5942cdc3b33fc75
susanahmed <susanahmed617@gmail.com>
2023-01-31 19:15:48 -0500

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
+import React from "react"
 
 function Keypad (){
+    function handleKeypad() {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div> <input type="password" onChange={handleKeypad} /> </div>
     )
 }
 
